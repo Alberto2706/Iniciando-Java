37c17f11f2730c2939926194eade5f2ce8383583
Alberto Lopez <ing_alberto.lopez@outlook.com>
2016-12-26 19:40:18 -0600

    26/12/2016 07:39pm
    Iniciando con los primeros ejemplos.

---
+++ src/Calculos_conMath.java
@@ -0,0 +1,14 @@
+
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		//double raiz=Math.sqrt(9);
+		//System.out.println("La raiz de 9 es: "+raiz);
+		
+		double num1=5.85;
+		int resultado = (int)Math.round(num1);
+		
+		System.out.println(resultado);
+	}
+
+}
