facd798f587d8157e7a34e714076cd9da69c8334
Alberto Lopez <ing_alberto.lopez@outlook.com>
2016-12-26 19:48:01 -0600

    Actualizando Calculos con Math 26/12/2016 07:49pm

--- src/Calculos_conMath.java
+++ src/Calculos_conMath.java
@@ -2,13 +2,20 @@
 public class Calculos_conMath {
 
 	public static void main(String[] args) {
+		//Primer ejemplo en Math, RAIZ CUADRADA______________________________________
 		//double raiz=Math.sqrt(9);
 		//System.out.println("La raiz de 9 es: "+raiz);
-		
+		/*_________REDONDEAR UN DECIMAL____________________________________________________
 		double num1=5.85;
 		int resultado = (int)Math.round(num1);
-		
 		System.out.println(resultado);
+		___________ELEVAR A UNA POTENCIA__________________________________________________*/
+		double base=5;
+		double exponente=3;
+		
+		int resultado = (int)Math.pow(base, exponente);
+		System.out.println(resultado);
+			
 	}
 
 }
